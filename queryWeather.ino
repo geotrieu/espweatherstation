d08d065b1feae2570ca3c74c731d3fbe4a4afaf8
geotrieu <geocrafterserver@gmail.com>
2019-02-12 15:43:11 -0500

    Fixed Weather Freezing Issue

--- queryWeather.ino
+++ queryWeather.ino
@@ -1,11 +1,8 @@
-int httpCode;
 const size_t bufferSize = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(1) + 2*JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(6) + JSON_OBJECT_SIZE(12) + 400;
-void queryWeather() {
-  httpCode = http.GET();
-  Serial.println(httpCode);                                                       
-  while (httpCode <= 0) {
-    Serial.print("!");
-    delay(20);
+void queryWeather() {                                                  
+  while (http.GET() <= 0) {
+    Serial.print("!HTTP!");
+    delay(100);
   }
   DynamicJsonBuffer jsonBuffer(bufferSize);
   JsonObject& root = jsonBuffer.parseObject(http.getString());
